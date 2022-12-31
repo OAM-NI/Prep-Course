4a69ee340995f4ff800b1e56e55487d2da5796ab
OAM-NI <120616257+OAM-NI@users.noreply.github.com>
2022-12-30 20:13:00 -0600

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -2,6 +2,7 @@
 
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = null;
+const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
